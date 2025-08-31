facc0530ee64d08528e6656b18bb10cd8342626a
Samuel Goh Jun Je <samueljjgoh@gmail.com>
2025-08-31 23:35:38 +0100

    Add environmental variables

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ from typing import List
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 app.config['CKEDITOR_SERVE_LOCAL'] = True 
 app.config['CKEDITOR_PKG_TYPE'] = 'standard' 
 app.config['CKEDITOR_CONFIG'] = {'versionCheck': False}
@@ -31,7 +35,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -269,5 +273,5 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=os.getenv("DEBUG") == "True", port=5002)
 
